540305d42aea6fa658324d56987e86a8d5186cdb
LucaGabi <37976898+LucaGabi@users.noreply.github.com>
2021-06-06 14:49:24 +0300

    Update sp.sh

--- sp.sh
+++ sp.sh
@@ -2,19 +2,19 @@
 
 #!/bin/bash
 clear
-USERS=`su – zimbra -c ‘zmprov -l gaa’`;
+USERS=`su - zimbra -c 'zmprov -l gaa'`;
 for ACCOUNT in $USERS; do
-ACC1=`echo $ACCOUNT | awk -F@ ‘{print $1}’`;
-ACC=`echo $ACC1 | cut -d ‘.’ -f1`;
+ACC1=`echo $ACCOUNT | awk -F@ '{print $1}'`;
+ACC=`echo $ACC1 | cut -d '.' -f1`;
 if [ $ACC != *"@amccrs.ro" ]; then
-echo “Skipping system account, $NAME…”;
+echo "Skipping system account, $NAME…";
 else
-echo “Modifying $ACCOUNT password…”;
-#su – zimbra -c “zmprov sp $ACCOUNT NewGeneratedPassword”;
-#su – zimbra -c “zmprov ma $ACCOUNT zimbraPasswordMustChange TRUE;
-echo “Done!”
-echo “”
+echo "Modifying $ACCOUNT password…";
+#su - zimbra -c "zmprov sp $ACCOUNT NewGeneratedPassword";
+#su - zimbra -c "zmprov ma $ACCOUNT zimbraPasswordMustChange TRUE;
+echo "Done!"
+echo ""
 # read anykey
 fi
 done
-echo “Modifying password for all user has been finished successfully”
+echo "Modifying password for all user has been finished successfully"
